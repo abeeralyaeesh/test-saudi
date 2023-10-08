d9d0bf498a105c50497bf5ba8d7caa3ed0dcc48c
AbeerAlyaeesh <abeer.alyaeesh3@gmail.com>
2023-10-08 09:51:34 +0300

    Initial Commit

---
+++ test saudi/test_saudiApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_saudiApp.swift
+//  test saudi
+//
+//  Created by Abeer Alyaeesh on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct test_saudiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test saudiUITests/test_saudiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test_saudiUITestsLaunchTests.swift
+//  test saudiUITests
+//
+//  Created by Abeer Alyaeesh on 23/03/1445 AH.
+//
+
+import XCTest
+
+final class test_saudiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
